75e2243d496528d59910b255610088628355389f
shata <shatalova.o.s@yandex.ru>
2023-05-12 11:37:21 +0300

    add models

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,21 @@
 package ru.practicum.shareit.booking;
 
+import lombok.Data;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDateTime;
+
 /**
  * TODO Sprint add-bookings.
  */
+@Data
 public class Booking {
+   private long id;
+   private LocalDateTime start;
+    private LocalDateTime end;
+    private Item item;
+    public User booker;
+    private BookStatus status;
 }
+
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,16 @@
 package ru.practicum.shareit.user;
 
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
 public class User {
+    private long id;
+    private String name;
+    @Email
+    private String email;
 }
