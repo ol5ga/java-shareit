6fef79584d2faaf975fd964eeaa1188c2d7abe0c
shata <shatalova.o.s@yandex.ru>
2023-06-25 22:13:13 +0300

    ItemRequestDtoTest

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.request.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
@@ -10,6 +11,7 @@ import javax.validation.constraints.Null;
  * TODO Sprint add-item-requests.
  */
 @Data
+@AllArgsConstructor
 public class ItemRequestDto {
     @NotBlank
     private String description;
